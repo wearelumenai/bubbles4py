64cdb333805b67150fbcce47f7c8df6b3cfb5905
ydarma <yves.darmaillac@gmail.com>
2019-06-26 14:16:53 +0200

    packaging

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='bubbles',
+    version='',
+    packages=['bubbles'],
+    install_requires=[
+          'bottle',
+    ],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
