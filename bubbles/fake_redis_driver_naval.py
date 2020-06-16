d8e2080778cb43d5867d713f3b63f7122ce835ba
paul <pgay@lumenai.fr>
2020-06-16 02:03:32 +0200

    naval in progress

---
+++ bubbles/fake_redis_driver_naval.py
@@ -0,0 +1,238 @@
+import copy
+import string, time
+import numpy as np
+import json
+from datetime import datetime
+from secrets import choice
+from threading import Lock
+import threading
+import networkx as nx
+from scipy.spatial import distance
+from munkres import Munkres
+from scipy import stats as s
+import pickle
+
+party_to_cat = {'em': 0,
+ 'indetermined': 1,
+ 'ps': 2,
+ 'lr': 3,
+ 'fn': 4,
+ 'fi': 5,
+ 'multi_affiliations': 6}
+
+topics = { 'colere': ['menace', 'attentat', 'mauvais','imposture', 'erreur','assez','guerre' 'violence', 'attaquer', 'attentat', 'police', 'escroquerie', 'manifestation', 'mensonge', 'mentir', 'systeme'], 'société': [ 'Retraités', 'islamistes', 'riches', 'supporter', 'exprimer', 'islamiste', 'europe', 'système'], 'economy': ['finance','économie', 'crise', 'entreprises', 'entreprise', 'bulle', 'libéral', 'classes', 'agriculture', 'chômeur', 'travail', 'euro', 'argent', 'milliard', 'dette'], 'macron': ['marche','emmanuel', 'macron', 'manuel'], 'ps': ['hollande', 'gauche', 'valls', 'benoît', 'socialiste'], 'republicains': ['fillon', 'sarkozy', 'juppé', 'penelope', 'droite', 'nicolas',  'pénélope', 'alain', 'françois'], 'insoumis': ['mélenchon', 'insoumis', 'luc', 'jean' ], 'rassembl.': ['marine', 'france'], 'positif' : ['soutien', 'gagn', 'avenir', 'soutenir', 'premier', 'bravo', 'allez', 'enfin', 'chance', 'rall', 'solution', 'mobilisation',  'confiance'] }
+key_topics = list(topics.keys())
+
+class Fake_redis_driver_naval:
+    """get some data, push then sequentially to grphclus, and read the result in the redis database which the grphclus storage"""
+
+    def __init__(self, pickle_containing_graph_and_coms, send_edge=True):
+        """
+        Create a new MemDriver instance
+        """
+        
+        data = pickle.load(open(pickle_containing_graph_and_coms, 'rb')) 
+        self.graph = data['graph']
+        self.communities = data['communities']
+        self.com_id = -1
+
+        self.time_idx = 0
+        self.dates = sorted(self.communities.keys()) #, key = lambda t: time.strptime(t, '%a %b %d %H:%M:%S +0000 %Y')) 
+        self.last_date = self.dates[self.time_idx]
+        self.level = 1
+        self.time_traveller_mode = False
+        self.present = 0
+
+    def put_result(self, result):
+        """
+        no implementation, because the redis database is filled by grphclus
+        """
+        return {}
+
+    def set_level(self, level):
+        self.com_id = -1
+        self.level = int(level)
+        num_of_communities = len([ n for n in community_tree.nodes if community_tree.nodes[n]['level'] == level ])
+        levels = set([community_tree.nodes[n]['level'] for n in community_tree.nodes ])
+        return len(levels) self.level, num_of_communities, self.com_id
+  
+    def get_num_levels(self):
+        levels = set([community_tree.nodes[n]['level'] for n in community_tree.nodes ])
+        num_of_levels = len(levels)
+        num_of_com = len([ n for n in community_tree.nodes if community_tree.nodes[n]['level'] == level ]) 
+        print("last date", self.last_date, 'index', self.time_idx, num_of_levels, self.level, num_of_com, self.com_id)
+        return num_of_levels, self.level, num_of_com, self.com_id
+
+    def set_date(self, date):
+        date = int(date)
+        if not self.time_traveller_mode:
+            self.present = self.time_idx # recall when is the present to go back
+            self.time_traveller_mode = True
+        print('traveled to', self.dates[date])
+        self.time_idx = date
+        if self.time_idx == self.present:
+            print('back to present')
+            self.time_traveller_mode = False # back to normal life
+        return self.get_result(0)
+
+    def put_result_id(self, result, result_id):
+        """
+        no implementation, the redis database is filled by grphclus
+        """
+        return {}
+ 
+    def get_community_detail(self):
+        pass
+
+    def communities_to_graph(self, communities):
+        graph = {"nodes": [], "links": []}
+
+        outgoing = dict([(k,set()) for k in communities])
+        import random
+        graph_size = random.randint(5,15)
+        biggest_com = sorted([ (len(v),k) for (k,v) in communities.items() ])[-graph_size:] # sorted()[-50:]
+        biggest_com = [k for (l,k) in biggest_com]
+        for k in biggest_com: #communities.items():
+            ns = communities[k]
+            graph["nodes"].append({"id":k, "name":k})
+            for n in ns:
+                outgoing[k].update(set(self.graph.neighbors(n)))
+        links = []
+        for k1 in biggest_com: #communities.items():
+            v1 = communities[k1]
+            for k2, v2 in outgoing.items():
+                if k1 != k2 and len(v1.intersection(v2)) > 0: 
+                    links.append((len(v1.intersection(v2)), k1, k2))
+        links = sorted(links)[-10:]
+        for w, k1, k2 in links:
+            graph['links'].append({"id": '_'.join(sorted([str(k1),str(k2)])),  "source": k1, "target": k2})
+        return graph
+
+    def get_result(self, result_id):
+        """
+        Ignoring the result_id actually, just read what is in the redis database 
+        """
+        self.last_date = self.dates[self.time_idx]
+        community_tree = self.communities[self.last_date]['community_tree']
+        communities = aggregate_childrens(community_tree, 2)
+        graph = {"nodes": [], "links": []}
+        nodes = [ {'id':n, 'value':len(communities[n])} for n in community_tree.nodes if community_tree.nodes[n]['level'] == level ]
+        links = [ for n in nodes for ne in neigbors ]
+        print('time',self.time_idx)
+        if self.com_id == -1:# ok in this condition, it makes sense to get communities
+            if self.level not in self.communities[self.last_date]:
+                #if self.time_idx < len(self.dates) - 1:
+                #    self.time_idx += 1
+                return {'centers': [], 'counts': [], 'columns': []}
+            communities = self.communities[self.last_date][self.level]['communities']
+
+        else:
+            communities = self.get_community_detail()
+        if self.time_idx < len(self.dates) - 1 and not self.time_traveller_mode:
+            self.time_idx += 1        
+        #result = json.load(open('/home/paul/programmation/lumenai/bubbles4py/examples/data_network.json'))
+        #result = json.load(open('/home/paul/programmation/lumenai/bubbles4py/twitter_graph.json'))
+        result = self.communities_to_graph(communities)
+        #print("returning", result)
+        print(result)
+        return result #self.results[result_id]['result']
+
+    """
+    def get_result(self, result_id):
+        self.time_idx += 1
+        nnodes = self.time_idx % 10 + 1
+        nodes = [{"id":i, "name":i} for i in range(nnodes) ]
+        links = [ {"id": '_'.join(sorted([str(n),str(n+1)])), "source":n, "target":n+1 } for n in range(nnodes-1)]
+        result = {"links":links, "nodes":nodes }
+        print(result)
+        return result
+    """
+
+    def detail_community(self, com_id):
+        self.com_id = int(com_id)
+        print('setting the detail on community',self.com_id)
+        return self.get_result(0) 
+
+    def get_results(self, start=None):
+        return {
+            k: _make_meta(v)
+            for k, v in self.results.items()
+            if start is None or v['created'] > start
+        }
+        
+    def get_last_date(self):
+        return self.last_date
+
+    def how_is_the_graph(self):
+        print('number of nodes', len(self.graph.nodes))
+        print('number of edges', len(self.graph.edges))
+
+def remap_centroids(centroids, last_centroids):
+    """
+    remap which hungarian method
+    """
+    m = np.zeros((len(centroids),len(last_centroids)))
+    ks = list(centroids.keys())
+    for i, k in enumerate(ks):
+      for j in range(len(last_centroids)): #enumerate(last_ks):
+        c1 = centroids[k]
+        c2 = last_centroids[j]
+        m[i,j] = distance.euclidean(np.array(c1), np.array(c2))
+    mu = Munkres()
+    if m.shape[0] > m.shape[1]:
+      indices = mu.compute(copy.copy(m.transpose()))
+      indices = dict([(j,i) for (i,j) in indices])
+    else:
+      indices = dict(mu.compute(copy.copy(m)))
+    ks_rearranged = [[]]*len(centroids)
+    to_add = set(range(len(ks))) # all of them should arrive in ks_rearranged
+    # add the ones which has been mapped by the hungarian algorithm
+    for i in range(len(centroids)):
+      if i in indices and indices[i]<len(centroids):
+        ks_rearranged[indices[i]] = ks[i]
+        to_add.remove(i)
+    """
+    print(centroids)
+    print('last',last_centroids)
+    print(ks_rearranged)
+    print('indices', indices)
+    """
+    # add the remaining ones
+    for i in range(len(centroids)):
+      if not ks_rearranged[i]: # if this one is empty #len(ks_rearranged[i]) == 0:
+        ks_rearranged[i] = ks[to_add.pop()]
+    return ks_rearranged
+
+
+def hashy(node):
+    int(node)
+    hashy = node // 100
+    key = hashy * 100 - node
+    return hashy, key
+
+def unhash(hashy, key):
+    return int(hashy) * 100 - int(key)
+
+
+def compute_entropy(data):
+    """
+    data is a matrix number_of_key_words X number_of_communities
+    data[i,j] : how many times the key_word i appears in the community j
+    """
+    row_normalise(data)
+    entropies = sum(data*log(data))
+    return entropies
+
+def load_data(f):
+    d = json.load(open(f))
+    return d
+    
+def _make_record(result):
+    return {
+        'result': result,
+        'created': datetime.now(),
+        'meta': result.get('meta')
+    }
+
+def _make_meta(record):
+    return {k: v for k, v in record.items() if k != 'result'}
