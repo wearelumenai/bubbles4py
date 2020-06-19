f436c0cae96e8c5553e94d1171c95a7fa047236f
b3j0f <ib3j0f@gmail.com>
2020-06-19 15:54:54 +0200

    fix(redis launcher)

--- examples/launch_fake_redis_driver.py
+++ examples/launch_fake_redis_driver.py
@@ -1,22 +1,39 @@
 import sys
 sys.path.append('/home/paul/programmation/lumenai/bubbles4py')
 
+import argparse
+
 from random import randint
 import time
 
-import bubbles.memdriver
+import bubbles.memdriver 
 #import MemDriver
-from bubbles import Server, fake_redis_driver
+from bubbles import Server, fake_redis_driver_naval
+
+def parseargs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', help='Random by default', default=randint(44001, 44999))
+    parser.add_argument('pickle', help='pickle file path')
+    return parser.parse_args()
 
 
-driver = fake_redis_driver.Fake_redis_driver("./storage.pck", send_edge=True)
-server = Server(driver)
-port = 44394 #randint(44001, 44999)
-if __name__ == "__main__":
-    server.start(port=port, quiet=True)
-    result_id = '00000'
-    print('http://localhost:{}/graph?result_id={}'.format(port, result_id))
+def main():
+    args = parseargs()
+
+    driver = fake_redis_driver_naval.Fake_redis_driver_naval(args.pickle, send_edge=True)
+
+    server = Server(driver)
+
+    server.start(port=args.port, quiet=True)
+    # result_id = '00000'
+    #print('http://localhost:{}/graph?result_id={}'.format(port, result_id))
+    print('http://localhost:{}/graph'.format(args.port))
+
     try:
         server.join()
     except KeyboardInterrupt:
         pass
+
+
+if __name__ == "__main__":
+    main()
